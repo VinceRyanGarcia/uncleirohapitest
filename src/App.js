6918d2e1934f8808002fc0117da6c27e39f7d128
Vince Garcia <vinceryangarcia@gmail.com>
2022-07-26 11:54:02 -0700

    css added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import tea from './images/tea.jpg'
 
 function App() {
 
@@ -14,23 +15,22 @@ function App() {
       .then( ( res ) => setData( res ) ) 
   },[])
 
-  console.log(data)
+  // console.log(data)
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>Uncle Iroh Quotes</h1>
+        <img src={tea} alt="uncle tea"></img>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-         <h1> {data.data.quote} </h1>
-         <h1> testing xd . </h1>
+          "{data.data?.quote}"
         </a>
       </header>
     </div>
